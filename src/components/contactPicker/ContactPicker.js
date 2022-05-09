dbcadce1c3a42cf6365e658f5a0372423c7ba527
DFKorn <d1man2192@mail.ru>
2022-05-10 00:22:26 +0300

    fix option keys

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -4,13 +4,13 @@ import React from "react";
 export const ContactPicker = ({name, contacts, onChange}) => {
   return (
     <select name={name} onChange={onChange}>
-      <option value="" key={-1}>
+      <option value={""} key={-1}>
         Please choose a contact
       </option>
       {contacts.map((contact) => {
          return (
-          <option value={contact} key={contact.name}>
-           {contact.name}
+          <option value={contact} key={contact}>
+           {contact}
           </option>
           );
       })}
